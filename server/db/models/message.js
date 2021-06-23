379477e6b6f31999c44137f3d19c344ab388e9b0
derek <wl328@njit.edu>
2021-06-23 03:53:29 -0400

    Set new attr for message

--- server/db/models/message.js
+++ server/db/models/message.js
@@ -10,6 +10,10 @@ const Message = db.define("message", {
     type: Sequelize.INTEGER,
     allowNull: false,
   },
+  readOrNot: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
 });
 
 module.exports = Message;
